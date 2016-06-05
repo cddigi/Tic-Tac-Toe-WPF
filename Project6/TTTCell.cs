619a112f43fe35711a5997c443c39bc68d6cf7a1
Cornelius Donley <donley@live.com>
2016-06-05 08:00:37 -0500

    Created TTTCell class.

---
+++ Project6/TTTCell.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Project6
+{
+    enum PlayerID { X, O, None };
+
+    class TTTCell : Label
+    {
+        public readonly int Row;
+        public readonly int Column;
+        public PlayerID Owner
+        {
+            get { return this.Owner; }
+            set
+            {
+                this.Owner = value;
+                switch(this.Owner)
+                {
+                    case PlayerID.X: this.Content = "X"; break;
+                    case PlayerID.O: this.Content = "O"; break;
+                    case PlayerID.None: this.Content = " "; break;
+                }
+            }
+        }
+
+        public TTTCell(int r, int c)
+        {
+            Row = r;
+            Column = c;
+            Owner = PlayerID.None;
+            this.FontSize = 50;
+            this.HorizontalContentAlignment = System.Windows.HorizontalAlignment.Center;
+            this.VerticalContentAlignment = System.Windows.VerticalAlignment.Center;
+            this.Content = " ";
+        }        
+    }
+}
